8fd1d156199f637dc84b6beeec8698b85ea624b4
unknown <Sreeram@.(none)>
2013-03-05 12:51:31 -0500

    bug fix

--- main.py
+++ main.py
@@ -10,14 +10,14 @@ class MainHandler(webapp.RequestHandler):
     def get (self, q):
         if q is None:
             q = 'index.html'
-     
-        path = os.path.join (os.path.dirname (__file__), q)
-        self.response.headers ['Content-Type'] = 'text/html'
-        self.response.out.write (template.render (path, {}))
-    
-    def main (self):
-        application = webapp.WSGIApplication ([('/(.*html)?', MainHandler)], debug=True)
-        util.run_wsgi_app (application)
-    
-    if __name__ == '__main__':
-        main ()
+
+            path = os.path.join (os.path.dirname (__file__), q)
+            self.response.headers ['Content-Type'] = 'text/html'
+            self.response.out.write (template.render (path, {}))
+
+def main ():
+    application = webapp.WSGIApplication ([('/(.*html)?', MainHandler)], debug=True)
+    util.run_wsgi_app (application)
+
+if __name__ == '__main__':
+    main ()
