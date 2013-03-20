df5f15a94f02390f2e38c693afa183fccf0254ec
unknown <Sreeram@.(none)>
2013-03-20 15:58:22 -0400

    update index and resume

--- main.py
+++ main.py
@@ -10,10 +10,9 @@ class MainHandler(webapp.RequestHandler):
     def get (self, q):
         if q is None:
             q = 'index.html'
-
-            path = os.path.join (os.path.dirname (__file__), q)
-            self.response.headers ['Content-Type'] = 'text/html'
-            self.response.out.write (template.render (path, {}))
+        path = os.path.join (os.path.dirname (__file__), q)
+        self.response.headers ['Content-Type'] = 'text/html'
+        self.response.out.write (template.render (path, {}))
 
 def main ():
     application = webapp.WSGIApplication ([('/(.*html)?', MainHandler)], debug=True)
