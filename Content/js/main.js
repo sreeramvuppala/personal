678330c353549cb739e87b424e0d467dda50d6ca
sreeramvuppala <sreeram.vuppala@gmail.com>
2015-03-24 19:08:05 -0400

    update

--- Content/js/main.js
+++ Content/js/main.js
@@ -2,7 +2,20 @@
  * @author Sreeram
  */
 $().ready(function(){
-	if(navigator.userAgent.toLowerCase().indexOf('chrome') > -1){		
+	if(navigator.userAgent.toLowerCase().indexOf('chrome') > -1){	
+
 	}
+
+	$("div.employment .inline-nav").click(function(){
+		var $self = $(this);
+		$("div.employment .page").addClass("hidden");
+		$("div.employment .page-" + $self.text()).removeClass("hidden");
+	});
+
+	$("div.skills .skills-nav").click(function(){
+		var $self = $(this);
+		$("div.skills .page").addClass("hidden");
+		$("div.skills .page-" + $self.text()).removeClass("hidden");
+	});
 });
 
